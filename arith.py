f94ebabba2162dd0285aa1462e03e39f5bce1ff9
dakshrajgoyal <dakshrajg@gmail.com>
2019-02-11 01:32:36 -0500

    first commit

---
+++ arith.py
@@ -0,0 +1,16 @@
+num1 =int (input('enter the first number'))
+num2 =int (input('enter the second number'))
+add = num1 +num2
+diff =num1-num2
+mul= num1*num2
+div= num1/num2
+floor_div =num1//num2
+power=num1 ** num2
+modulus= num1%num2
+print('sum of ',num1, 'and',num2,' is:', add)
+print('subtraction of ',num1,'and',num2,' is:', diff)
+print('multiplication of',num1, 'and',num2,' is :', mul)
+print('division of ',num1, 'and',num2 ,'is:', div)
+print('floor division of ',num1, 'and', num2, 'is:', floor_div)
+print('power of ',num1, 'and', num2, 'is:', power)
+print('modulus of ',num1, 'and', num2, 'is:', modulus)
